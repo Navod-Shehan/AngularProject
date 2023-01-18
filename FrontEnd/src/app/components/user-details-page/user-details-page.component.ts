8a7b5a5af60bb0f78ee893d130e5ca4a50e05bea
Navod Wimalaweera <navod@ebuilder.local>
2023-01-18 08:39:22 +0530

    popup create

--- FrontEnd/src/app/components/user-details-page/user-details-page.component.ts
+++ FrontEnd/src/app/components/user-details-page/user-details-page.component.ts
@@ -1,82 +1,175 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+
 import { MatPaginator } from '@angular/material/paginator';
+
 import { MatSort } from '@angular/material/sort';
+
 import { MatTableDataSource } from '@angular/material/table';
+
 import { HttpClient } from '@angular/common/http';
+
 import { UsersService } from 'src/app/services/users.service';
 
+
+
 export interface UserData {
+
   id: string;
+
   email: string;
+
   username: string;
+
   firstName: string;
+
   city: string;
-  action:any[];
+
+  action: any[];
+
 }
 
 
+
 @Component({
+
   selector: 'app-user-details-page',
+
   templateUrl: './user-details-page.component.html',
-  styleUrls: ['./user-details-page.component.scss']
+
+  styleUrls: ['./user-details-page.component.scss'],
+
 })
-export class UserDetailsPageComponent implements  OnInit {
+
+export class UserDetailsPageComponent implements OnInit {
+
+  //init table columns
+
+  displayedColumns: string[] = [
+
+    'id',
+
+    'email',
+
+    'username',
+
+    'firstName',
+
+    'city',
+
+    'action',
+
+  ];
 
 
 
-  displayedColumns: string[] = ['id', 'email', 'username', 'firstName', 'city','action'];
+  //init table data source
+
   dataSource!: MatTableDataSource<UserData>;
+
+
+
+  //init variables
+
   users: any[] = [];
 
+  userDetails: any;
+
+  address: any;
+
+  phoneNumber: any;
+
+  name: any;
 
 
 
   @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+
   @ViewChild(MatSort, { static: false }) sort!: MatSort;
-test: any;
 
-  constructor(private usersService: UsersService) {
 
-  }
+
+  constructor(private usersService: UsersService) {}
+
 
 
   ngOnInit() {
+
+    //retrieve users from the api
+
     this.usersService.GetUsers().subscribe((data: any) => {
+
       console.log(data);
+
       this.users = data;
+
       this.dataSource = new MatTableDataSource(this.users);
 
-      this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-    }
 
-    );
+
+      this.dataSource.paginator = this.paginator;
+
+      this.dataSource.sort = this.sort;
+
+    });
+
   }
 
+
+
+  //filter table section
 
 
 
   applyFilter(event: Event) {
+
     const filterValue = (event.target as HTMLInputElement).value;
+
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
+
+
     if (this.dataSource.paginator) {
+
       this.dataSource.paginator.firstPage();
+
     }
+
   }
 
 
-  popup(user:  any){
+
+  //popup section
+
+  popup(user: any) {
+
     console.log(user.phone);
-    this.test =JSON.stringify(user) ;
-    console.log(this.test);
-    for (var key of Object.keys(this.test)) {
-      console.log(key + " -> " + this.test[key])
- }
 
-    console.log("test popup ")
+    this.userDetails = JSON.stringify(user.address);
+
+    this.address = JSON.stringify(user.address);
+
+    this.phoneNumber = JSON.stringify(user.phone);
+
+    this.name = JSON.stringify(user.name);
+
+
+
+    //use for testings
+
+
+
+    //     console.log(this.userDetails);
+
+    //     for (var key of Object.keys(this.userDetails)) {
+
+    //       console.log(key + " -> " + this.userDetails[key])
+
+    //  }
+
+
+
+    //     console.log("test popup ")
+
   }
+
 }
-
-
-
